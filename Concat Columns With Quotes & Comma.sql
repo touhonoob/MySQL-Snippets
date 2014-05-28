289bbe29055da2739dd04e5d4d80fb7e3884dabb
komica2 <komica@dr.com>
2014-05-29 02:37:29 +0800

    Update Concat Columns With Quotes & Comma.sql

--- Concat Columns With Quotes & Comma.sql
+++ Concat Columns With Quotes & Comma.sql
@@ -1,4 +1,4 @@
-select concat_ws(concat('\'',column_name,'\''),',') 
+select group_concat(concat('\'',column_name,'\'')) 
 from information_schema.columns 
 where table_name='{table_name}' 
 and table_schema='{table_schema}';
